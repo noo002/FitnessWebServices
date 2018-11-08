260883192056764119ca03288982b7b4ffab659a
Asus <Asus@192.168.0.4>
2018-11-08 23:31:06 +0800

    this is for mobile side

---
+++ Model/activityLogDa.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'Connection.php';
+require_once '../Model/activityLog.php';
+
+class activityLogDa {
+
+    public function registerNewActivityLog($activityLog) {
+        //    private $type, $description, $duration, $distance, $traineeId;
+        $conn = Connection::getInstance();
+        $sqlInserted = "call registerNewActivitylog(?,?,?,?,?)";
+        $stmt = $conn->getDb()->prepare($sqlInserted);
+        $type = $activityLog->type;
+        $description = $activityLog->description;
+        $duration = $activityLog->duration;
+        $distance = $activityLog->distance;
+        $traineeId = $activityLog->traineeId;
+        $stmt->bindParam(1, $type);
+        $stmt->bindParam(2, $description);
+        $stmt->bindParam(3, $duration);
+        $stmt->bindParam(4, $distance);
+        $stmt->bindParam(5, $traineeId);
+        try {
+            $result = $stmt->execute();
+            return $result;
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+
+}
+
+//$activityLog = new activityLog(1, 1, 222, 5.221, 1);
+//$da = new activityLogDa();
+//$result = $da->registerNewActivityLog($activityLog);
